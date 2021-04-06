693f8b5ee53076b864dd814f3f39efa239ee411d
Vincent Colavin <vcolavin@gmail.com>
2021-04-06 11:17:53 -0700

    import employee data properly

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      <p>hello and welcome to the party!</p>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,13 @@
+import employees from "../data/employees.json";
+
+function Wrapper() {
+  const test = employees;
+  debugger;
+  return (
+    <div className="wrapper">
+      <p>hello and welcome to the party!</p>
+    </div>
+  );
+}
+
+export default Wrapper;
